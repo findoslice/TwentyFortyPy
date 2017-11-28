3caadc2d233e74ea400352405dc1989dbaa9a615
findlaysmith <ferelach45@gmail.com>
2017-11-28 03:49:49 +0000

    made the code DRYer and added more core methods

--- twentyfortypy.py
+++ twentyfortypy.py
@@ -12,17 +12,31 @@ class TwentyFortyPy:
                       ((3,0), None), ((3,1), None), ((3,2), None), ((3,3), None)
                      ]
         for i in range(2):
-            while True:
-                newNode = self.board[self.random.randint(0,15)]
-                if newNode[1] == None:
-                    newVal = self.random.choice([2,2,4])
-                    for j in range(len(self.board)):
-                        if self.board[j][0] == newNode[0]:
-                            print (newNode[0])
-                            self.board = self.board[:j] + [(self.board[j][0], newVal)] + (self.board[j+1:] or [])
-                    break
-                else:
-                    pass
+            self.addRandValue()
     
     def currentState(self):
         return self.board
+    
+    def changeValue(self, coords, newvalue):
+        coords = coords
+        newvalue = newvalue
+        for j in range(len(self.board)):
+                        if self.board[j][0] == coords:
+                            self.board = self.board[:j] + [(coords, newvalue)] + (self.board[j+1:] or [])
+
+    def addRandValue(self):
+        while True:
+                newNode = self.board[self.random.randint(0,15)]
+                if newNode[1] == None:
+                    newVal = self.random.choice([2,2,2,2,4])
+                    self.changeValue(newNode[0], newVal)
+                    break
+                else:
+                    pass
+
+if __name__ == "__main__":
+    board = TwentyFortyPy()
+    print (board.currentState())
+    print ("\n\n")
+    board.changeValue((0,0), 16)
+    print (board.currentState())
